aee85b32b85e21bfffec8d61eabdd032d4bd2623
yeahieakhan <yeahieakhan2017@gmail.com>
2026-04-08 14:39:23 +0600

    updated

---
+++ src/app/nextLevel/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function page() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <Button>Shadcn Button</Button>
+      <h2>This is next level page</h2>
+    </div>
+  );
+}
